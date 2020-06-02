008341ef24511a34d0558efcf0812206cc1a321f
IrinaAlcala <58370677+IrinaAlcala@users.noreply.github.com>
2020-06-02 15:38:47 -0700

    Updates

---
+++ precache-manifest.966156d3d41b8603f4639e727c1e29ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd8b869041b520177499d332f06d7e1e",
+    "url": "//github.com/IrinaAlcala/project-4-assessment/index.html"
+  },
+  {
+    "revision": "024208f901b5ced8fd66",
+    "url": "//github.com/IrinaAlcala/project-4-assessment/static/css/main.23cf19b8.chunk.css"
+  },
+  {
+    "revision": "3357667a71712448bd75",
+    "url": "//github.com/IrinaAlcala/project-4-assessment/static/js/2.2018531e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//github.com/IrinaAlcala/project-4-assessment/static/js/2.2018531e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "024208f901b5ced8fd66",
+    "url": "//github.com/IrinaAlcala/project-4-assessment/static/js/main.c2ba81df.chunk.js"
+  },
+  {
+    "revision": "bbb270662f22171e3e63",
+    "url": "//github.com/IrinaAlcala/project-4-assessment/static/js/runtime-main.8127fb64.js"
+  }
+]);
